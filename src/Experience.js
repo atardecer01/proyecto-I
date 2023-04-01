c0e530a8ad5891f776cf53520b172caed464a4d2
Lina Marcela Rivas <70968546+atardecer01@users.noreply.github.com>
2023-03-31 23:05:58 -0600

    actualizacion

--- src/Experience.js
+++ src/Experience.js
@@ -1,13 +1,15 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Html, Text,Float } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import WarriorModel from './WarriorModel'
 import { MeshStandardMaterial, Scene, TextureLoader } from 'three'
 import { useLoader } from '@react-three/fiber'
+import { Suspense, useRef } from 'react'
 
 export default function Experience()
 {
 
     const stone = useLoader(TextureLoader, './images/areni.jpg')
+  
     return <>
 
         <Perf position="top-left" />
@@ -21,13 +23,47 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <WarriorModel>
-            <MeshStandardMaterial/>
-        </WarriorModel>
-           
+        <Suspense
+        fallback={
+            <mesh>
+                <boxGeometry args={[1,1,1,2,2,2]} />
+                <meshBasicMaterial wireframe color='grey' />
+            </mesh>
+        }
+        >
+            <WarriorModel/>
+        </Suspense>
+        
         <mesh position-y={-0.15} rotation-x={-Math.PI*0.5} scale ={10}>
             <planeGeometry args={[1.2]}/>
             <meshStandardMaterial map = {stone} />
         </mesh>
+        <Html
+            position={[0,0.2,0]}
+            wrapperClass='label'
+            center
+            distanceFactor={12}
+            fontSize ={1}
+        >
+            Lucharon Con Honor
+        </Html>
+        <Float
+            speed={2.5}
+            floatIntensity={2}
+            position={[-0.5,-1.6,3]}
+        >
+             <Text
+            font='./RiverStonePersonalUse-qZaE1.otf'
+            fontSize={0.5}
+            color= 'white'
+            position-y={2}
+            masWidth= {1}
+            textAlign = 'center'
+            distanceFactor={8}
+            >
+                Soldados De Terracota
+            </Text> 
+        </Float>
+      
     </>
 }
